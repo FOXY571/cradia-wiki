d6b0b820ad698ad30f40d3408ebea95cf55e4330
FOXY_571 <thomastowson2007@gmail.com>
2025-02-17 19:11:39 -0600

    Updated 404 entry

--- scripts/contentLoader.js
+++ scripts/contentLoader.js
@@ -9,12 +9,14 @@ if (entryName && entries.contains(entryName)) {
   contentBody.innerHTML = await entries.getEntry(entryName);
 } else {
   contentBody.innerHTML = `
-    <p>It looks like you steered off course! Unfortunately, There is no entry for
-      <i>${entries.fomratEntryName(entryName)}.</i>
-      :(
+    <h1>404</h1>
+    <div class="line-break"></div>
+
+    <p>
+      It looks like you steered off course! Unfortunately, There is no entry for <i>${entries.fomratEntryName(entryName)}.</i> :(
     </p>
-    <p>Please click
-      <b><a href="?entry=home">here</a></b>
-    to go back to safety.</p>
+    <p>
+      Please click <b><a href="?entry=home" title="Home">here</a></b> to go back to safety.
+    </p>
   `;
 }
