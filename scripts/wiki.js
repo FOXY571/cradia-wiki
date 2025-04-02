58406a7a3b40780b8504ecb559f992d1e943537c
FOXY_571 <thomastowson2007@gmail.com>
2025-04-02 17:58:13 -0500

    Added heading, heading buttons, & source viewing button to the wiki

--- scripts/wiki.js
+++ scripts/wiki.js
@@ -11,3 +11,16 @@ document.querySelector('.js-random-page-link')
   .addEventListener('click', () => {
     window.location.href = `?entry=${getRandomEntryName()}`;
   });
+
+const sourceButton = document.querySelector('.js-source-button');
+if (entryName.endsWith('-source')) {
+  sourceButton.innerHTML = `<a class="header-link" title="View the page">View page</a>`;
+  sourceButton.addEventListener('click', () => {
+    window.location.href = `?entry=${entryName.replace('-source', '')}`;
+  });
+} else {
+  sourceButton.innerHTML = `<a class="header-link" title="View the page's source">View source</a>`;
+  sourceButton.addEventListener('click', () => {
+    window.location.href = `?entry=${entryName}-source`;
+  });
+}
