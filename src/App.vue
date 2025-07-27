4e4804a577a2fcd6f4a4241eee473db0479e1379
Thomas Towson <thomastowson2007@gmail.com>
2025-07-27 10:46:37 -0500

    Added main page

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,302 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+<div class="background"></div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+<div class="logo">
+  <a href="?entry=home" title="Visit the main page"></a>
+</div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+<div class="content-wrapper">
+  <div class="panels">
+    <div class="panel wiki-content main-border">
+      <p class="panel-label">Navigation</p>
+      <ul class="panel-list">
+        <li>
+          <a class="panel-link" href="?entry=home" title="Visit the main page">
+            <span>Main page</span>
+          </a>
+        </li>
+        <li>
+          <a class="panel-link" href="?entry=all-pages" title="Look at all pages">
+            <span>All pages</span>
+          </a>
+        </li>
+        <li>
+          <a id="randomPageLink" class="panel-link" title="Load a random page">
+            <span>Random page</span>
+          </a>
+        </li>
+        <li>
+          <a class="panel-link" href="?entry=help" title="Get some help">
+            <span>Help</span>
+          </a>
+        </li>
+      </ul>
     </div>
-  </header>
 
-  <RouterView />
+    <div class="panel wiki-content main-border">
+      <p class="panel-label">Books</p>
+      <ul class="panel-list">
+        <li>
+          <a class="panel-link" href="https://docs.google.com/document/d/1SCAqx2TM08QJp9W8waC3sXVsQbn6opX7FLpoLhBMtJM/edit?usp=sharing" target="_blank" title="Read The Prince's Truth">
+            <span>The Prince's Truth</span>
+          </a>
+        </li>
+        <li>
+          <a class="panel-link" href="https://docs.google.com/document/d/1znis8jsUf_rI-8urWsdnmdusVwqlymZG8xuqvJfN74Y/edit?usp=sharing" target="_blank" title="Read Heyvan">
+            <span>Heyvan</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  
+  <div class="main-panel wiki-content main-border">
+    <div class="header">
+      <div class="header-buttons">
+        <div id="sourceButton" class="source-button">
+          <a class="header-link" title="View the page's source">View source</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="main-content">
+      <div id="contentBody" class="content-body"></div>
+      <div class="side-spacer"></div>
+    </div>
+  </div>
+</div>
+
+<footer class="footer">
+  <p>
+    Website created by
+    <a class="footer-link" href="https://github.com/FOXY571" target="_blank">Thomas Towson</a>
+  </p>
+</footer>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.background {
+  position: fixed;
+
+  background-image: url("./assets/images/background.jpg");
+  background-size: cover;
+  background-position: center;
+  filter: blur(3px);
+  
+  transform: scale(1.1);
+  height: 100%;
+  width: 100%;
+  z-index: -1;
 }
 
 .logo {
+  background-image: url("./assets/images/logo.png");
+  background-repeat: no-repeat;
+  background-position: 50%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 175px;
+}
+
+.logo a {
+  width: 325px;
+  height: 50%;
+}
+
+.content-wrapper {
+  flex: 1;
+
+  display: flex;
+  gap: 10px;
+
+  margin: 25px;
+  margin-top: 0;
+}
+
+.wiki-content {
+  background: url("./assets/images/content-background.jpg");
+  background-repeat: repeat-y;
+  background-size: 100%;
+
+  box-shadow: 2.5px 2.5px 10px 0 rgba(10, 10, 10, 0.7); 
+}
+
+.panels {
+  background-size: unset;
+
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.panel {
+  background-size: unset;
+
+  width: 200px;
+  height: min-content;
+}
+
+.panel-label,
+.header {
+  background-color: rgba(0, 0, 0, 0.15);
+
+  border-color: rgb(20, 20, 20);
+  border-style: solid;
+  border-width: 0 0 1px 0;
+  border-radius: 5px 5px 0 0;
+
+  font-size: 17px;
+  line-height: 10px;
+
+  margin: 0;
+  padding: 10px;
+}
+
+.panel-list {
+  border-top: 1px solid rgba(255, 255, 255, 0.1);
+
+  list-style-type: none;
+
+  margin: 0;
+  padding: 0;
+}
+
+.panel-link {
+  color: var(--link-color);
+  font-size: 14px;
+  text-decoration: none;
+
   display: block;
-  margin: 0 auto 2rem;
+  
+  margin: 7px;
+  padding: 7px;
+
+  transition: background 0.3s;
+  cursor: pointer;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.panel-link:hover,
+.header-link:hover {
+  background-color: rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.panel-link:active,
+.header-link:active {
+  text-decoration: underline;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.main-panel {
+  flex: 1;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.header-buttons {
+  display: flex;
+  justify-content: right;
+
+  margin: 0;
+  margin-right: 300px;
 }
 
-nav a:first-of-type {
-  border: 0;
+.header-link {
+  color: var(--link-color);
+  font-size: 14px;
+  text-decoration: none;
+  
+  margin: 0px;
+  padding: 7px;
+
+  transition: background 0.3s;
+  cursor: pointer;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+.main-content {
+  border-top: 1px solid rgba(255, 255, 255, 0.1);
+
+  display: flex;
+  overflow: hidden;
+}
+
+.content-body {
+  flex: 1;
+  overflow: auto;
+
+  height: min-content;
+  padding: 20px;
+}
+
+.side-spacer {
+  width: 300px;
+}
+
+.footer {
+  background-color: var(--background-color);
+
+  display: flex;
+  justify-content: center;
+}
+
+.footer-link {
+  color: var(--primary-color);
+  text-decoration: none;
+}
+
+.footer-link:hover {
+  text-decoration: underline;
+}
+
+@media (max-width: 1250px) {
+  .side-spacer {
+    width: 0;
   }
 
+  .header-buttons {
+    margin-right: 0;
+  }
+}
+
+@media (max-width: 900px) {
   .logo {
-    margin: 0 2rem 0 0;
+    background-size: auto 75%;
+
+    height: 135px;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .content-wrapper {
+    flex-direction: column-reverse;
+    justify-content: start;
   }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+  .panels {
+    flex-direction: row;
+  }
 
-    padding: 1rem 0;
-    margin-top: 1rem;
+  .main-content {
+    flex: 0;
+  }
+}
+
+@media (max-width: 600px) {
+  .logo {
+    background-size: auto 65%;
+
+    height: 100px;
+  }
+}
+
+@media screen and (max-width: 450px) {
+  .panels {
+    flex-direction: column;
+  }
+
+  .panel {
+    width: auto;
   }
 }
 </style>
